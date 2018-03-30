aec6b15b357e671fa1ba2dbb7de36193d882fbc6
Hansel <hanselcapillo@hotmail.com>
2018-03-30 16:25:05 -0400

    Obteniendo la información del formulario

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-template',
@@ -7,14 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+
+    usuario: Object = {
+        nombre: 'Fernnado',
+        apellido: 'Herrera',
+        email: 'hansel@hotmail.com'
+    };
+
     constructor() { }
 
     ngOnInit() {
     }
 
 
-    guardar() {
+    guardar(forma: NgForm) {
         console.log('Formulario guardado');
+        console.log('NgForm', forma);
+        console.log('Valor', forma.value);
+
+        console.log('Usuario', this.usuario);
     }
 
 }
