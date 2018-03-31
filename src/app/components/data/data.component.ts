6bf8756806f6595e0919e573e90ca52bf9a63adf
Hansel <hanselcapillo@hotmail.com>
2018-03-31 11:14:39 -0400

    Data: Continuando con las validaciones y HTML

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-data',
-  templateUrl: './data.component.html',
-  styles: []
+    selector: 'app-data',
+    templateUrl: './data.component.html',
+    styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
+    /*Definicion del formulario*/
+    forma: FormGroup;
 
-  constructor() { }
+    constructor() {
+        /* El FormControl recibde tres parametros
+        1.valor
+        2.validaciones
+        3. validaciones asincronas */
+        this.forma = new FormGroup({
+            'nombre': new FormControl('Gernando', Validators.required),
+            'apellido': new FormControl('', Validators.required),
+            'email': new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$')])
+        });
+    }
 
-  ngOnInit() {
-  }
+
+    guardarCambios() {
+        console.log(this.forma);
+        console.log(this.forma.value);
+    }
+
+
 
 }
